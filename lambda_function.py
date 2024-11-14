bd08bc7ad593aecc1a25843d2fd6a3c803237c8a
Eduardo Corrêa <125585961+edu-correa@users.noreply.github.com>
2024-11-14 15:10:10 -0300

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,12 +1,13 @@
 import json
 import boto3
+import base64
 
 def lambda_handler(event, context):
     # TODO implement
-    bucket_name = 'bucket-scrt'
+    bucket_name = 'bucket-scrt-1'
     s3 = boto3.resource('s3')
     objeto = s3.Object(bucket_name, event['nomeArquivo'] + '.txt')
-    fileTxt = json.dumps(event['dadosArquivo'])
+    fileTxt = bytearray(event['dadosArquivo'])
     
     objeto.put(Body=fileTxt)
     return {
